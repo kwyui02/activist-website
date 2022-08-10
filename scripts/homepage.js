92d0da3c5c47561333dfbeea0dbb072d62776e32
Ashley Franchesca <kwyui02@gmail.com>
2022-08-10 21:07:18 +0800

    ver 5.1 added quote generator

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -10,3 +10,25 @@ menuToggle.addEventListener("click", () => {
     menuToggle.classList.toggle("active")
     showcase.classList.toggle("active")
 })
+
+var quoteList = [
+    "Climate Change is the defining issue of our time and we are at a defining moment. From shifting weather patterns that threaten food production, to rising sea levels that increase the risk of catastrophic flooding, the impacts of climate change are global in scope and unprecedented in scale. - United Nations",
+    "The world must come together to confront climate change. There is little scientific dispute that if we do nothing, we will face more drought, famine and mass displacement that will fuel more conflict for decades. - Barack Obama",
+    "We don’t have time to sit on our hands as our planet burns. For young people, climate change is bigger than election or re-election. It’s life or death. - Alexandria Ocasio-Cortez",
+    "We simply must do everything we can in our power to slow down global warming before it is too late... The science is clear. The global warming debate is over. - Arnold Schwarzenegger"
+]
+
+var myBtn = document.getElementById("myBtn");
+var quote = document.getElementById("quote");
+var count = 0;
+
+myBtn.addEventListener("click", displayQuote);
+
+function displayQuote() {
+    quote.innerHTML = quoteList[count];
+    count += 1;
+
+    if (count >= quoteList.length) {
+        count = 0;
+    }
+}
